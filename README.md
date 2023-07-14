21cf22a3d002ad2ed15f9fa068609be3f75a0fb0
VaishaliA12 <92015324+VaishaliA12@users.noreply.github.com>
2023-07-14 13:45:14 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# MoodMusic
+This is MoodMusic developed by-
+1. **Chetali Pushkarna** (https://github.com/Chetalipushkarna) 
+2. **Vaishali Agarwal** () 
+3. **Jasmeen Kaur** ()  
+4. **Khushboo** (https://github.com/Khushboo1806)
+
+-----------------------------------------------------------------
+
+**Steps of Run this project in your system:**
+
+
+1.Download this project
+2. Open terminal and install below libraries.
+    i. pip install flask  
+    ii. pip install chatterbot  
+    iii. pip install textblob  
+    iv. pip install ntlk  
+4. Open app.py Run this project.
---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from textblob import TextBlob
+import webbrowser
+count = 0
+
+app = Flask(__name__)
+
+english_bot = ChatBot("ChatterBot", storage_adaptor="chatterbot.storage.SQLStorageAdaptor")
+trainer = ChatterBotCorpusTrainer(english_bot)
+
+trainer.train("chatterbot.corpus.english")
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/get")
+def get_bot_response():
+    global count
+    userText = request.args.get('msg')
+    obj = TextBlob(userText)
+    sentiment, subjectivity = obj.sentiment
+    print(obj.sentiment)
+    if sentiment > 0:
+        count = count + 1
+    elif sentiment < 0:
+        count = count - 1
+    else:
+        count = count + 0
+    print("count", count)
+
+    return str(english_bot.get_response(userText))
+
+
+
+@app.route("/forward/", methods=['POST'])
+def get_song_playlist():
+    global count
+    if count > 0:
+        webbrowser.open_new("https://wynk.in/music/playlist/desi-hip-hop-hits/bb_1491457072469?q=hip+hop")
+        return "Here's a playlist of desi hip-hop hits!"
+    elif count < 0:
+        webbrowser.open_new("https://wynk.in/music/playlist/arijit-all-the-way/QVVESU9fQXJpaml0IFNpbmdoIC0gQWxsIFRoZSBXYXlfMjAxNF9zcmNoX2JzYl9oaQ?q=+")
+        return "Here's a playlist of Arijit all the way!"
+    else:
+        webbrowser.open_new("https://wynk.in/music/playlist/motivational-tunes-hindi/bb_1588754763579?q=motivation")
+        return "Here's a playlist of motivational tunes in Hindi!"
+if __name__ == '__main__':
+    app.run()
+
+
+
